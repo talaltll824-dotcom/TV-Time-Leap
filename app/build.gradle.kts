0840caece50bc938c3f1e74e61a704f60cac6594
talaltll824-dotcom <talaltll824@gmail.com>
2026-09-17 21:48:40 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.tvtimeleap.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tvtimeleap.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-ui:1.5.1")
+}
