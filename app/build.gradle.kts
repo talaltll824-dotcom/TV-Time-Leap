c8ef94ff861f75516b7f0afdbceee098fdce82db
talaltll824-dotcom <talaltll824@gmail.com>
2026-09-18 01:49:37 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,12 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tvtimeleap.app"
+        applicationId = "com.tvtimeleap.timeshift"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 2
+        versionName = "2.0"
     }
 
     compileOptions {
@@ -28,6 +29,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-ui:1.5.1")
 }
