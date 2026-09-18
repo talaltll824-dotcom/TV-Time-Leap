91a8dfa3fc7bf72ac0fe9a9ba5d3553a2d708fdc
talaltll824-dotcom <talaltll824@gmail.com>
2026-09-18 03:41:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tvtimeleap.channeltest20260918"
+        applicationId = "com.tvtimeleap.finaltest20260918"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
